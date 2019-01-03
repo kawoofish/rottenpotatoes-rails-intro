ec4bb4938391dc61ef8a9066defd76dc0343f163
Alex Le <kawoofish@gmail.com>
2019-01-03 01:27:28 +0000

    Part 2 of HW completed

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,4 @@
 class MoviesController < ApplicationController
-  helper_method :hilight
   
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date)
@@ -12,10 +11,17 @@ class MoviesController < ApplicationController
   end
 
   def index
+    @all_ratings = Movie.order(:rating).select(:rating).map(&:rating).uniq
+    @checked_ratings = check
+    
+    @checked_ratings.each do |rating|
+      params[rating] = true
+    end
+    
     if params[:sort]
       @movies = Movie.order(params[:sort])
     else
-      @movies = Movie.all
+      @movies = Movie.where(:rating => @checked_ratings)
     end
   end
 
@@ -45,6 +51,14 @@ class MoviesController < ApplicationController
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
+  end
+  
+  def check
+    if params[:ratings]
+      params[:ratings].keys
+    else
+      @all_ratings
+    end
   end
   
 end
